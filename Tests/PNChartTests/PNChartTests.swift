f5c85413cae572d4a102ad6b42fe5dbfbd82c7d9
damonchen <netubu@gmail.com>
2021-08-30 18:22:04 +0800

    add PNChart project

--- Tests/PNChartTests/PNChartTests.swift
+++ Tests/PNChartTests/PNChartTests.swift
@@ -6,6 +6,6 @@
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(PNChart().text, "Hello, World!")
+//            XCTAssertEqual(PNChart().text, "Hello, World!")
         }
     }
